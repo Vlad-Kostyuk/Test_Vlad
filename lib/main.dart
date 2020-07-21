4e51e25372c1379cb4485457b2a82a4be4b968fe
Vlad-Kostyuk <vlad.kostyuk24@gmail.com>
2020-07-21 14:36:12 +0300

    create project and add start page

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:testvlad/pages/start_page.dart';
+
+void main() {
+  runApp(MainPage());
+}
+
---
+++ lib/pages/start_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+         // title: ,
+        ),
+      ),
+    );
+  }
+}
